c01545756124dcff9f60f29876215ee1c9265204
jfsolerma <jfsolerma@gmail.com>
2023-03-14 19:32:53 +0100

    Create app springinitialzr

---
+++ cartmanagement/src/test/java/com/onebox/cartmanagement/CartmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebox.cartmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
